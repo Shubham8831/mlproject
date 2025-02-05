9197c757335b3202f96e9488d02b00b369ca0869
shubham8831 <shubuamsingh2002@gmail.com>
2025-02-05 16:10:11 +0530

    eda and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 #THIS WILL BE COMMON IN ALL THE PROJECTS
 import sys #any exception that is getting control sys will have that info
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
